51c44386262ce05521cd7f209fa12669b925f806
akshay-varun <akshaygr4@gmail.com>
2020-05-19 16:27:15 +0530

    Added header.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Typist from 'react-typist';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+
+        <div className="header">
+            <a href="#default" className="logo">Logo</a>
+            <div className="header-right">
+                <a className="active" href="#home">Home</a>
+                <a href="#contact">About</a>
+                <a href="#about">Contact</a>
+            </div>
+        </div>
+
       <header className="App-header">
 
 
